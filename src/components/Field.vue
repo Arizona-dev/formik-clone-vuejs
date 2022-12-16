a77a001bb8449c3a88eda39ef1341934d3cb3cfd
omaziarz <omaziarz.pro@gmail.com>
2022-12-16 13:57:42 +0100

    add field

---
+++ src/components/Field.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  component: {
+    type: String,
+    required: false,
+    default: "input",
+  },
+  type: {
+    // define type as html input type
+    type: String,
+    required: false,
+    default: "text",
+  },
+  value: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <component
+    :is="component"
+    :type="type"
+    :value="value"
+    :checked="value"
+    @input="$emit('update:value', $event)"
+  >
+    <slot></slot>
+  </component>
+</template>
